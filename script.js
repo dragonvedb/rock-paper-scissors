ec386e22319fe99b7999e628cc600ca21ab38907
Daniel Bronsky <dragonvedb@gmail.com>
2022-10-31 22:17:18 +0300

    Declare a function to generate computer's stance

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    
+    switch(computerChoice) {
+        case 0:
+            computerChoice = 'Rock';
+            break;
+        case 1:
+            computerChoice = 'Paper';
+            break;
+        case 2:
+            computerChoice = 'Scissors';
+            break;
+    }
+
+    return computerChoice;
+    }
