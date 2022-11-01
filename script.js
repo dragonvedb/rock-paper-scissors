6ce3847b3323abdfb383206a8615956d20fb2d24
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-01 18:40:10 +0300

    Add game round functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+ function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     
     switch(computerChoice) {
@@ -15,3 +15,27 @@ function getComputerChoice() {
 
     return computerChoice;
     }
+
+    function playRound(computerChoice, playerChoice) {
+        let winMsg = `You won! ${playerChoice} beats ${computerChoice}.`
+        let loseMsg = `You lost. ${computerChoice} beats ${playerChoice}.`
+        
+        switch(playerChoice) {
+            case computerChoice:
+                return `It's a tie! You both played ${computerChoice}.`;
+
+            case 'Rock':
+                return (computerChoice === 'Scissors') ? winMsg : loseMsg;
+
+            case 'Paper':
+                return (computerChoice === 'Rock') ? winMsg : loseMsg;
+
+            case 'Scissors':
+                return (computerChoice === 'Paper') ? winMsg : loseMsg;
+        }
+    }
+
+    const computerChoice = getComputerChoice();
+    const playerChoice = 'Rock';
+    console.log(computerChoice)
+    //console.log(playRound(computerChoice, playerChoice))
