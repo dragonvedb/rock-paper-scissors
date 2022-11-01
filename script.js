94325992ad88a2d6c1d184fc466c7845a5eeecee
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-01 19:56:51 +0300

    Turn the player input string to Abc capitalization scheme

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// THIS IS A SINGLE GAME ROUND.
+// TAKE PLAYER'S AND COMP'S CHOICES AND RETURN WIN\LOSE\TIE MEASSAGE.
 function playRound(computerChoice, playerChoice) {
+    
+    // pregenerate win and lose messages
     let winMsg = `You won! ${playerChoice} beats ${computerChoice}.`
     let loseMsg = `You lost. ${computerChoice} beats ${playerChoice}.`
         
+    // this switch returns and win/lose message or a tie message in case the choices are the same
     switch(playerChoice) {
         case computerChoice:
             return `It's a tie! You both played ${computerChoice}.`;
@@ -35,11 +40,16 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
+// THIS CONTAINS AN ENTIRE GAME SESSION CONSISTING OF FIVE ROUNDS.
+// EVERY ROUND IT GENERATES A COMPUTER CHOICE AND ASKS THE PLAYER FOR ONE.
+// THEN IT PLAYS A ROUND AND ADJUST THE SCORE. AFTER FIVE ROUNDS DECLARES A WINNER.
 function game() {
     for (let i = 1; i <= 1; i++) {
         const computerChoice = getComputerChoice();
         const playerChoice = function() {
-            return prompt("What do you pick?");
+            let input = prompt("What do you pick?").toLowerCase();
+            let firstChar = input.charAt(0);
+            return firstChar.toUpperCase() + input.substring(1);
         };
         alert(playerChoice());
     }
